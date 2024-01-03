f02db7443046e185802c24e187126c36c96c53c1
Mario Theers <mtheers@slashwhy.de>
2024-01-03 20:45:32 +0100

    did ex. 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -24,28 +24,31 @@ const Total = ({num_excercises}) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  let num_exercises= parts.map(p => p.exercises)
-                          .reduce((partialSum, a) => partialSum + a)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  let num_exercises= course.parts.map(p => p.exercises)
+                                 .reduce((partialSum, a) => partialSum + a)
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts} />      
+      <Header course={course.name}/>
+      <Content parts={course.parts} />      
       <Total num_excercises={num_exercises} />
     </div>
   )

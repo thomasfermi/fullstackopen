1c3bf9cc0334daa50ea20904ff4c8738ed116fff
Mario Theers <mtheers@slashwhy.de>
2024-01-03 20:43:20 +0100

    did ex. 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,16 +3,18 @@ const Header = ({course}) => {
   return <h1>{course}</h1>
 }
 
-const Part = ({part_name, exercises}) => {
-  return <p> {part_name} {exercises} </p>
+const Part = ({part}) => {
+  return <p> {part.name} {part.exercises} </p>
 }
 
-const Content = (props) => {
+const Content = ({parts}) => {
+  const part_components = parts.map((part, index) => (
+    <Part key={index} part={part} />
+  ));
+
   return (
     <div>
-      <Part part_name={props.part1.name} exercises={props.part1.exercises}/>
-      <Part part_name={props.part2.name} exercises={props.part2.exercises}/>
-      <Part part_name={props.part3.name} exercises={props.part3.exercises}/>
+      {part_components}
     </div>
   )
 }
@@ -23,24 +25,28 @@ const Total = ({num_excercises}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  let num_exercises= parts.map(p => p.exercises)
+                          .reduce((partialSum, a) => partialSum + a)
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total num_excercises={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />      
+      <Total num_excercises={num_exercises} />
     </div>
   )
 }

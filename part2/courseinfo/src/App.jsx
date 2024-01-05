d17d1d2a5cf38799dea114840da9b995122d7c06
Mario Theers <mtheers@slashwhy.de>
2024-01-05 20:28:56 +0100

    did ex. 2.1 and 2.2 and 2.3

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,60 @@
+
+const Header = ({course}) => {
+  return <h1>{course}</h1>
+}
+
+const Part = ({part}) => {
+  return <p> {part.name} {part.exercises} </p>
+}
+
+const Content = ({parts}) => {
+  const part_components = parts.map((part, index) => (
+    <Part key={index} part={part} />
+  ));
+
+  return (
+    <div>
+      {part_components}
+    </div>
+  )
+}
+
+const Total = ({num_excercises}) => {
+  return <p>Number of exercises {num_excercises}</p>
+}
+
+const Course = ({course}) => {
+  let num_exercises= course.parts.map(p => p.exercises)
+                                 .reduce((partialSum, a) => partialSum + a)
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts} />      
+      <Total num_excercises={num_exercises} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App

260ecb07a9a4c84838b50dc705df11d6db872eaf
Mario Theers <mtheers@slashwhy.de>
2024-01-05 20:34:05 +0100

    did ex. 2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -8,8 +8,8 @@ const Part = ({part}) => {
 }
 
 const Content = ({parts}) => {
-  const part_components = parts.map((part, index) => (
-    <Part key={index} part={part} />
+  const part_components = parts.map(part => (
+    <Part key={part.id} part={part} />
   ));
 
   return (
@@ -20,7 +20,7 @@ const Content = ({parts}) => {
 }
 
 const Total = ({num_excercises}) => {
-  return <p>Number of exercises {num_excercises}</p>
+  return <p><b>Number of exercises {num_excercises}</b></p>
 }
 
 const Course = ({course}) => {
@@ -36,25 +36,56 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+      {courses.map(course => <Course key={course.id} course={course}/>)}
+    </div>
+  )
 }
 
 export default App

de4bc4f6abaec5693b3f004e505368d0ac5e5a0b
Mario Theers <mtheers@slashwhy.de>
2024-01-10 09:30:02 +0100

    did ex. 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const Person = ({person}) => {
+  //console.log("Person:", person)
+  return (
+    <div>{person.name}</div>
+  )
+}
+
+const Numbers = ({persons}) => {
+  //console.log("Persons:", persons)
+  const rendered_persons = persons.map(p => <Person key={p.id} person={p}/>)
+  
+  //console.log(rendered_persons)
+  return (
+    <div>
+    <h2>Numbers</h2>
+    {rendered_persons}
+    </div>  
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id:0 }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {name : newName, id : persons.length}
+    setPersons(persons.concat(newPerson))
+    setNewName("")
+  }
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <Numbers persons={persons}/>
+    </div>
+  );
+};
+
+export default App;

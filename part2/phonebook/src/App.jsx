75c7918599872a09c476a515e369029cb1b2afcb
Mario Theers <mtheers@slashwhy.de>
2024-01-10 09:44:55 +0100

    did ex. 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,9 +3,16 @@ import { useState } from "react";
 const Person = ({person}) => {
   //console.log("Person:", person)
   return (
-    <div>{person.name}</div>
+    <div>{person.name} {person.number}</div>
   )
 }
+
+function isPhoneNumber(input) {
+  const phoneRegex = /^[0-9]+(-[0-9]+)*$/;
+  return phoneRegex.test(input) && !input.startsWith('-') && !input.endsWith('-') && input.indexOf('--') === -1;
+}
+
+
 
 const Numbers = ({persons}) => {
   //console.log("Persons:", persons)
@@ -21,19 +28,32 @@ const Numbers = ({persons}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", id:0 }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1234567", id:0 }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("0");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
   const addPerson = (event) => {
     event.preventDefault()
-    const newPerson = {name : newName, id : persons.length}
+    const newPerson = {name : newName, number : newNumber, id : persons.length}
+    if (newName === "")
+    {
+      alert("You did not enter a name.")
+    }
     if (persons.some(person => person.name === newName))
     {
-      alert(`${newName} is already added to phonebook`)
+      alert(`${newName} is already added to phonebook.`)
+    }
+    else if (!isPhoneNumber(newNumber))
+    {
+      alert(`${newNumber} is not a valid phone number.`)
     }
     else {
       setPersons(persons.concat(newPerson))
@@ -48,6 +68,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>

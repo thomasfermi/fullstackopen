9fce0366393844a2371c284529bb7a0f91242bdd
Mario Theers <mtheers@slashwhy.de>
2024-01-10 09:55:58 +0100

    did ex. 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,36 +1,50 @@
 import { useState } from "react";
 
-const Person = ({person}) => {
+const Person = ({ person }) => {
   //console.log("Person:", person)
   return (
-    <div>{person.name} {person.number}</div>
-  )
-}
+    <div>
+      {person.name} {person.number}
+    </div>
+  );
+};
 
 function isPhoneNumber(input) {
   const phoneRegex = /^[0-9]+(-[0-9]+)*$/;
-  return phoneRegex.test(input) && !input.startsWith('-') && !input.endsWith('-') && input.indexOf('--') === -1;
+  return (
+    phoneRegex.test(input) 
+  );
 }
 
-
-
-const Numbers = ({persons}) => {
+const Numbers = ({ persons, searchName }) => {
   //console.log("Persons:", persons)
-  const rendered_persons = persons.map(p => <Person key={p.id} person={p}/>)
-  
+  const nameFitsSearchName = (name) => {
+    return name.toLowerCase().includes(searchName)
+  }
+  const rendered_persons = persons
+    .filter((p) => nameFitsSearchName(p.name))
+    .map((p) => <Person key={p.id} person={p} />);
+
   //console.log(rendered_persons)
   return (
     <div>
-    <h2>Numbers</h2>
-    {rendered_persons}
-    </div>  
-  )
-}
+      <h2>Numbers</h2>
+      {rendered_persons}
+    </div>
+  );
+};
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1234567", id:0 }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 0 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 1 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 2 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 3 },
+  ]);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("0");
+  const [searchName, setSearchName] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -40,31 +54,32 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
-  const addPerson = (event) => {
-    event.preventDefault()
-    const newPerson = {name : newName, number : newNumber, id : persons.length}
-    if (newName === "")
-    {
-      alert("You did not enter a name.")
-    }
-    if (persons.some(person => person.name === newName))
-    {
-      alert(`${newName} is already added to phonebook.`)
-    }
-    else if (!isPhoneNumber(newNumber))
-    {
-      alert(`${newNumber} is not a valid phone number.`)
-    }
-    else {
-      setPersons(persons.concat(newPerson))
-      setNewName("")
-    }    
-  }
+  const handleSearchNameChange = (event) => {
+    setSearchName(event.target.value);
+  };
 
+  const addPerson = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName, number: newNumber, id: persons.length };
+    if (newName === "") {
+      alert("You did not enter a name.");
+    }
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook.`);
+    } else if (!isPhoneNumber(newNumber)) {
+      alert(`${newNumber} is not a valid phone number.`);
+    } else {
+      setPersons(persons.concat(newPerson));
+      setNewName("");
+    }
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      Filter for name:{" "}
+      <input value={searchName} onChange={handleSearchNameChange} />
+      <h2>Add a new number</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -76,7 +91,7 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <Numbers persons={persons}/>
+      <Numbers persons={persons} searchName={searchName} />
     </div>
   );
 };

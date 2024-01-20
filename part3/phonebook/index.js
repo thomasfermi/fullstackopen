efd6a1f09172b1ff67894e47b85b9eadc84b1075
Mario Theers <mtheers@slashwhy.de>
2024-01-20 13:36:34 +0100

    did ex. 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,37 +1,51 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 let persons = [
-    {
-      "id": 1,
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": 2,
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": 3,
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": 4,
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    }
-]
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+app.get("/info", (request, response) => {
+  const line1 = `<p>Phonebook has info for ${persons.length} people.</p>`;
+  const options = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    timeZoneName: "long",
+  };
+  const currentDate = new Date().toLocaleString("en-GB", options);
+  const line2 = `<p>${currentDate}</p>`;
+  const html_str = `<html>${line1}\n${line2}</html>`;
+  response.send(html_str);
+});
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});

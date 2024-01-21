ad5dc117618e5b394a53dd76f1f96ee8dd37b629
Mario Theers <mtheers@slashwhy.de>
2024-01-21 15:16:30 +0100

    added cors (hence did ex. 3.9)

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 var morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(
   morgan(function (tokens, req, res) {
     return [

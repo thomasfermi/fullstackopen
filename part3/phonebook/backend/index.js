4d66e98478ff8c456c5d446da7357af2284265c5
Mario Theers <mtheers@slashwhy.de>
2024-01-23 19:54:35 +0100

    did ex. 3.11

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 var morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
 app.use(

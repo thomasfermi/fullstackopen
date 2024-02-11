8ddb54062dd2bec1a74c464f8aa5b88b01dde43f
Mario Theers <mtheers@slashwhy.de>
2024-02-11 09:31:46 +0100

    did ex. 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const sum_reducer = (sum, item) => {
+    return sum + item
+  }
+
+  return blogs.map((b) => b.likes).reduce(sum_reducer, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }

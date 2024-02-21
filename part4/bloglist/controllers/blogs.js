227c64e3e7b7e7cd25547f4b581e7c9eb36da854
Mario Theers <mtheers@slashwhy.de>
2024-02-21 10:45:44 +0100

    did ex. 4.8

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,20 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const logger = require('../utils/logger')
 
-blogsRouter.get('/', (request, response) => {
-  logger.info('ab')
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+  const result = await blog.save()
+  response.status(201).json(result)
 })
 
 module.exports = blogsRouter

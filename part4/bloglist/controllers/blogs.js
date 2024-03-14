fb3eb7888c10c8000917d901730e1d80f1a5e1d3
Mario Theers <mtheers@slashwhy.de>
2024-03-14 14:12:23 +0100

    did ex. 4.13

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -23,4 +23,13 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(result)
 })
 
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
+})
+
 module.exports = blogsRouter
--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -62,37 +62,55 @@ test('adding a blog works', async () => {
   expect(response.body).toHaveLength(initialBlogs.length + 1)
 }, 100000)
 
-test('omitting likes works', async () => {
-  // add blog
-  const newBlog = {
-    title: 'Cheese is tasty',
-    author: 'Mickey Mouse',
-    url: 'https://cheese.com/',
-  }
-  await api.post('/api/blogs').send(newBlog)
+describe('when adding a blog', () => {
+  test('omitting likes works', async () => {
+    // add blog
+    const newBlog = {
+      title: 'Cheese is tasty',
+      author: 'Mickey Mouse',
+      url: 'https://cheese.com/',
+    }
+    await api.post('/api/blogs').send(newBlog)
 
-  // assert
-  const response = await api.get('/api/blogs')
-  expect(response.body[2].likes).toEqual(0)
-}, 100000)
+    // assert
+    const response = await api.get('/api/blogs')
+    expect(response.body[2].likes).toEqual(0)
+  }, 100000)
 
-test('omitting title does not work', async () => {
-  const newBlog = {
-    author: 'Mickey Mouse',
-    url: 'https://cheese.com/',
-    likes: 77,
-  }
-  await api.post('/api/blogs').send(newBlog).expect(400)
-}, 100000)
+  test('omitting title does not work', async () => {
+    const newBlog = {
+      author: 'Mickey Mouse',
+      url: 'https://cheese.com/',
+      likes: 77,
+    }
+    await api.post('/api/blogs').send(newBlog).expect(400)
+  }, 100000)
 
-test('omitting url does not work', async () => {
-  const newBlog = {
-    title: 'Cheese is tasty',
-    author: 'Mickey Mouse',
-    likes: 77,
-  }
-  await api.post('/api/blogs').send(newBlog).expect(400)
-}, 100000)
+  test('omitting url does not work', async () => {
+    const newBlog = {
+      title: 'Cheese is tasty',
+      author: 'Mickey Mouse',
+      likes: 77,
+    }
+    await api.post('/api/blogs').send(newBlog).expect(400)
+  }, 100000)
+})
+
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = (await api.get('/api/blogs')).body
+    const blogToDelete = blogsAtStart[0]
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+    const blogsAtEnd = (await api.get('/api/blogs')).body
+
+    expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1)
+
+    const blogTitles = blogsAtEnd.map((r) => r.title)
+
+    expect(blogTitles).not.toContain(blogToDelete.title)
+  })
+})
 
 afterAll(async () => {
   await mongoose.connection.close()

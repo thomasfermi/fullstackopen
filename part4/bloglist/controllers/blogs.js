460d75dc666d38d7548a32b6b199b84d50329106
Mario Theers <mtheers@slashwhy.de>
2024-03-21 14:50:58 +0100

    did ex. 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,8 +1,14 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const log = require('../utils/logger')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {
+    username: 1,
+    name: 1,
+    id: 1,
+  })
   response.json(blogs)
 })
 
@@ -17,10 +23,17 @@ blogsRouter.post('/', async (request, response) => {
   if (!('title' in submittedBlog)) {
     return response.status(400).json({ error: 'title is required' })
   }
+  const user = (await User.find({}))[0]
+  submittedBlog.user = user._id
+  log.info('submittedBlog:')
+  log.info(submittedBlog)
   const blog = new Blog(submittedBlog)
+  const savedBlog = await blog.save()
 
-  const result = await blog.save()
-  response.status(201).json(result)
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
